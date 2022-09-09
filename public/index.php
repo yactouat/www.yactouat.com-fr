e6978fbeff4584d3b45047ad1b87161ec493ca04
yactouat <yactouat@hotmail.com>
2022-09-09 23:33:18 +0200

    testing conf in entry point

--- public/index.php
+++ public/index.php
@@ -5,5 +5,11 @@ $rootDir = dirname(__DIR__);
 
 // loading deps
 require_once $rootDir."/vendor/autoload.php";
+use App\Conf;
+
+if (!Conf::checkDevConf() || !Conf::checkSharedConf()) {
+    http_response_code(500);
+    die("conf KO");
+}
 
 echo "it works".PHP_EOL;
