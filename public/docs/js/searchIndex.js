eede4ed360cac763e5246244e2f63ee7e837c20f
yactouat <yactouat@hotmail.com>
2022-09-10 22:05:02 +0200

    updating PHP docs for  and

--- public/docs/js/searchIndex.js
+++ public/docs/js/searchIndex.js
@@ -16,6 +16,26 @@ Search.appendIndex(
             "summary": "checks\u0020if\u0020shared\u0020PHP\u0020conf.\u0020corresponds\u0020to\u0020what\u0027s\u0020expected",
             "url": "classes/App-Conf.html#method_checkSharedConf"
         },                {
+            "fqsen": "\\App\\Constants",
+            "name": "Constants",
+            "summary": "",
+            "url": "classes/App-Constants.html"
+        },                {
+            "fqsen": "\\App\\Constants\u003A\u003AAPP_ENV",
+            "name": "APP_ENV",
+            "summary": "",
+            "url": "classes/App-Constants.html#constant_APP_ENV"
+        },                {
+            "fqsen": "\\App\\Constants\u003A\u003ADEV_ENV",
+            "name": "DEV_ENV",
+            "summary": "",
+            "url": "classes/App-Constants.html#constant_DEV_ENV"
+        },                {
+            "fqsen": "\\App\\Constants\u003A\u003APROD_ENV",
+            "name": "PROD_ENV",
+            "summary": "",
+            "url": "classes/App-Constants.html#constant_PROD_ENV"
+        },                {
             "fqsen": "\\",
             "name": "\\",
             "summary": "",
